568f812c8af72cfa1f119446998e2ec07e53e423
Linus Mårtensson <linus.martensson@gmail.com>
2017-08-13 21:43:47 +0200

    Update

--- server/cores/002-static-cache.js
+++
@@ -1,6 +0,0 @@
-
-import staticCache from 'koa-static-cache'
-
-module.exports = (app)=>{
-	app.staticCache = staticCache('public', {prefix:'/public'});
-}
--- server/middleware/400-koa-router.js
+++ server/middleware/400-koa-router.js
@@ -4,23 +4,29 @@ import Router from 'koa-router'
 module.exports = (app) => {
 	var r = new Router();
 
-	r.redirect('/','/public/index.html');
-	
-	r.get('/public/*', app.staticCache);
-
 	r.get('/details', ctx => {
 		console.dir(ctx);
 		console.dir("IO")
 		console.dir(app.io);
+		ctx.body = "Hello!"
 	})
 
+	r.post('/register', (ctx, next) => {
+
+	});
 	r.post('/login', (ctx, next) => {
 		return app.passport.authenticate('local', {
-			successRedirect: '/public/index.html',
+			successRedirect: '/token',
 			failureRedirect: '/'
 		})(ctx, next)
 	});
 
+	r.get('/status', (ctx, next) => {
+		//Get complete current user status
+	});
+	r.get('/content', (ctx, next) => {
+
+	});
 
 	app.koa.use(r.routes());
 	app.koa.use(r.allowedMethods());
