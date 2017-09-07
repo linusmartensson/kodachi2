4464e309a7fbc5c62adb22d720a67acf90c4dbdd
Linus Mårtensson <linus.martensson@gmail.com>
2017-09-07 21:26:20 +0200

    Add key to pages

--- client/src/Pages.js
+++ client/src/Pages.js
@@ -6,7 +6,7 @@ import StoredPage from './StoredPage'
 
 class Pages extends Component {
   render() {
-    const books = this.props.books.map((book) => <StoredPage name={book.title} path={book.path} />);
+    const books = this.props.books.map((book) => <StoredPage key={book.id} name={book.title} path={book.path} />);
     return (<div className="Pages">{books}</div>);
   }
 }
