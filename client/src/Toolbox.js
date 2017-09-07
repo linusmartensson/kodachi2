8aca7d951c18e311637c2af5559e1179a21d983e
Linus Mårtensson <linus.martensson@gmail.com>
2017-09-07 21:26:04 +0200

    Fix tool key

--- client/src/Toolbox.js
+++ client/src/Toolbox.js
@@ -8,7 +8,7 @@ class Toolbox extends Component {
   render() {
     
     const tools = this.props.tools.map((tool) => 
-        <Tool name={tool.title} />
+        <Tool key={tool.id} name={tool.title} />
     );
 
     return (<div className="Toolbox">{tools}</div>);
