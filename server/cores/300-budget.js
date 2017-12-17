9e16d20076ec013e264c16627cf64d3819972777
Linus Mårtensson <linus.martensson@gmail.com>
2017-12-17 01:29:38 +0100

    Fix budget account creation

--- server/cores/300-budget.js
+++ server/cores/300-budget.js
@@ -5,7 +5,7 @@ module.exports = async (app) => {
     var api = {};
 
     api.addGroup = async (event, type, limit) => {
-        await app.cypher('MATCH (e:Event {id:{event}}) CREATE (:BudgetGroup {type:{type}, limit:{limit}, total:0})-->e', {event, type, limit});
+        await app.cypher('MATCH (e:Event {id:{event}}) CREATE (:BudgetGroup {type:{type}, limit:{limit}, total:0})-->(e)', {event, type, limit});
     }
 
     api.addBudget = async (event, type, value) => {
