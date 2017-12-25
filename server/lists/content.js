9dacf42f355fc4f9572db3a9927991f5243ffb0f
Linus Mårtensson <linus.martensson@gmail.com>
2017-12-25 23:35:04 +0100

    Prepare content for edit

--- server/lists/content.js
+++ server/lists/content.js
@@ -14,12 +14,21 @@ module.exports = (app) => {
         //for teams
         for(var v in pages.records){
 
+
             var page = pages.records[v].get('w').properties;
+            page.name = page.title;
+            page.access = [];
+
+            var access = await app.cypher("MATCH (w:Content {id:{id}})<-[:HAS_ACCESS]-(r:Role) RETURN r", {id:page.id});
+            for(var w in access.records){
+                page.access.push(access.records[w].get('r').properties.type);
+            }
+            page.edit = true;
 
             var r = {tiers:[{
                 id:0, panels:[
                     {id:0, content:[{id:0, type:'text', text:page.title}]},
-                    {id:2, content:[{id:0, type:'editbutton', text:"Edit page", task:'create_page', data:{content:page}}]}
+                    {id:2, content:[{id:0, type:'editbutton', text:"Edit page", tool:'create_page', data:page}]}
                 ]
             }], id:0};
 
