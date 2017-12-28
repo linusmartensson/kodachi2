0254cf16ef6c366b6b471fa1030b1d013d1a6270
Linus Mårtensson <linus.martensson@gmail.com>
2017-12-28 23:38:49 +0100

    email bugfix

--- server/cores/203-users.js
+++ server/cores/203-users.js
@@ -145,7 +145,7 @@ module.exports = async (app) => {
         if(!html) html = text;
         u = u[0].get('u').properties;
         var lang = app.userApi.getUserLanguage(u);
-        await app.utils.email(u.email, app.stringApi.get_string(subject, lang), app.stringApi.get_string(text, lang), app.stringApi.get_string(html, lang));
+        await app.utils.email(u.email, app.stringApi.parse(subject, lang), app.stringApi.parse(text, lang), app.stringApi.parse(html, lang));
     }
     api.findAccount = async (p) => {
         var q = [];
