22f9481bf91950cda26baa3f78e2e2477fd58ca4
Linus Mårtensson <linus.martensson@gmail.com>
2017-12-29 04:12:09 +0100

    Update infopopup

--- client/src/InfoPopup.js
+++ client/src/InfoPopup.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import SpeechBubble from './SpeechBubble'
 import './InfoPopup.css'
+import {connect} from 'react-redux'
 class InfoPopup extends Component {
   render() {
 
+    if(!this.props.msg) return null;
     return (
             <div className="InfoPopup">
               <div className="InfoPopup-inner">
-                <SpeechBubble speaker="/img/kodachi_lores2.png" text="Hey! There's a thing that happened!!" />
+                <SpeechBubble speaker="/img/kodachi_lores2.png" text={this.props.msg} />
               </div>
             </div>);
   }
 }
 
-export default InfoPopup;
+export default connect(state => ({msg: state.currentMessage}),dispatch=>({}))(InfoPopup);
