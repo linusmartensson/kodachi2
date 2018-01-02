07ba9737b978cdb8d9e3395be454fa08d30f42df
Linus Mårtensson <linus.martensson@gmail.com>
2018-01-02 20:00:10 +0100

    Add https redirect

--- server/cores/900-koa-listen.js
+++ server/cores/900-koa-listen.js
@@ -2,11 +2,16 @@ import Koa from 'koa';
 import winston from 'winston'
 import https from 'https'
 import http from 'http'
+import koa from 'koa'
 var tools = require('../tools/core')
 
 module.exports = async (app) => {
 
 	app.koa.listen(3001);
+
+    app.redirect = new Koa();
+    app.redirect.use(require('koa-sslify')());
+    app.redirect.listen(3002);
 
 	winston.info("Started koa!");
 
