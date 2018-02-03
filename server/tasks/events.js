15e1987bf665b323ebc367202c0acbe50aeda9c1
Linus Mårtensson <linus.martensson@gmail.com>
2018-02-03 19:09:23 +0100

    Add crew admin role

--- server/tasks/events.js
+++ server/tasks/events.js
@@ -43,7 +43,7 @@ module.exports = async (app) => {
                     const w = q.get("u").properties;
                     v.values.push(w.email);
                 }
-            }}, {field: "type", type: "dropdown", values: ["admin", "budget", "schedule", "artist_alley_admin", "vendor_admin", "team_admin", "activity_admin"]}
+            }}, {field: "type", type: "dropdown", values: ["admin", "budget", "schedule", "artist_alley_admin", "vendor_admin", "team_admin", "activity_admin", "crew_admin"]}
         ),
         async (inst, ctx) => {
             if (inst.response.cancel) {
