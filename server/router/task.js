e819363ead3621f32ed3808724bc1c57af9af675
Linus Mårtensson <linus.martensson@gmail.com>
2018-11-27 14:06:23 +0100

    New task response format

--- server/router/task.js
+++ server/router/task.js
@@ -5,12 +5,16 @@ module.exports = (app) => {
     const r = new Router();
 
     r.post("/start_task/:task_name", async (ctx, next) => {
-        ctx.body = await app.taskApi.start_task(ctx, ctx.params.task_name, ctx.request.body);
+        ctx.body = {};
+        ctx.body.response = await app.taskApi.start_task(ctx, ctx.params.task_name, ctx.request.body);
+        ctx.body.state = await app.sessionApi.buildSession(ctx) 
     });
 
     r.post("/respond_task/:task_id", async (ctx, next) => {
         const form = await app.utils.form(ctx);
-        ctx.body = await app.taskApi.respond_task(ctx, ctx.params.task_id, form);
+        ctx.body = {};
+        ctx.body.response = await app.taskApi.respond_task(ctx, ctx.params.task_id, form);
+        ctx.body.state = await app.sessionApi.buildSession(ctx);
     });
     return r;
 
