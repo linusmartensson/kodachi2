7a1d9161851459b22b77cbd20bd00b4e6b512c79
Linus Mårtensson <linus.martensson@gmail.com>
2019-01-02 18:00:28 +0100

    Reduce booked count when adding team member

--- server/tasks/activities.js
+++ server/tasks/activities.js
@@ -269,7 +269,7 @@ module.exports = async (app) => {
         q.id = user.id;
         q.team = inst.data.start_data.team;
 
-        await app.cypher("MATCH (u:User {id:{id}}), (t:WorkGroup {id:{team}}) CREATE (u)-[:TEAM_MEMBER {sleep:{sleep_at_event}, wednesday:{can_work_wednesday}, sunday:{can_cleanup_sunday}, tshirt:{tshirt}}]->(t)", q);
+        await app.cypher("MATCH (u:User {id:{id}}), (t:WorkGroup {id:{team}}) CREATE (u)-[:TEAM_MEMBER {sleep:{sleep_at_event}, wednesday:{can_work_wednesday}, sunday:{can_cleanup_sunday}, tshirt:{tshirt}}]->(t) SET t.booked -= (case exists(t.booked) and toInt(t.booked)>0 when true then 1 else 0 end", q);
         await app.roleApi.addRole(inst.origin, `team_member.${inst.data.start_data.event_id}`, 3000);
         await app.roleApi.addRole(inst.origin, `team_member.${q.team}`);
         await app.roleApi.addAchievement(q.id, "joined_a_team", 1, app.userApi.getActiveEvent(ctx), 1, 10);
