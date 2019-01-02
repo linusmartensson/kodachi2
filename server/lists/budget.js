17fd26b449121541dd20874b73a828c2c888764e
Linus Mårtensson <linus.martensson@gmail.com>
2019-01-02 17:55:32 +0100

    Switch to receipt list in admin_budget

--- server/lists/budget.js
+++ server/lists/budget.js
@@ -1,5 +1,13 @@
 module.exports = (app) => {
-    app.listApi.build_list("budget", "admin_budget", ["admin", "team_admin.", "event_admin.", "crew_admin."], 
-        "MATCH (w:WorkGroup)--(e:Event {id:{event}}) with w.type as type, collect(w) as groups, sum(toInt(w.budget)) as total unwind groups as w return w, total order by type, w.name", ["w", "total"], {event:'inst.start_data.event_id'}, {event_list: true, group_by:'w.type'});
+
+    app.listApi.build_list(
+        "budget", 
+        "admin_budget", 
+        ["admin"], 
+        "MATCH (r:Receipt)--(u:User) return r,u",
+        ["r", "u"],
+        {event:'inst.start_data.event_id'}, 
+        {event_list:true});
+
 
 };
