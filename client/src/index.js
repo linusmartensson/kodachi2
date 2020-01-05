4d2ea3430f1017ee43140267e31a2a8ffbbf0a15
Linus Mårtensson <linus.martensson@gmail.com>
2020-01-05 23:14:04 +0100

    unregister service workers

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from 'react-snapshot';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker'
 import './index.css';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
